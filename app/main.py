032dcf64077980cb75792c39e7f0300755d421d2
yg763877 <yg763877>
2026-05-02 09:29:31 +0200

    Updated the API
    Added new different paths and tested the OpenAPI docs.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from app.models.user import User
 
 app = FastAPI()
+
+@app.get("/")
+def welcome():
+    return {"message": "Welcome to the Webapp."}
+
+@app.post("/register/")
+async def regitser(user: User):
+    print("Welcome to the registration process.")
+    return user
+
+@app.get("/login")
+def login():
+    return {"message": "Please Log in."}
+
+@app.get("/users")
+def get_users():
+    return {"message": "Welcome to the users page."}
