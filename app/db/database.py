172826805d56daf3ea1b674fbf77aa6202d5554a
= <=>
2026-05-17 15:43:51 +0200

    Udated DB
    added delete user function and improved existing functions

--- app/db/database.py
+++ app/db/database.py
@@ -27,33 +27,39 @@ def close_db():
     return {"Connection closed"}
 
 # Register a new user
-def add_user (username, pwd):
-    cur.execute(sql_insert, (username, pwd))
+def add_user_db (username, pwd):
+    cur.execute(sql_check, (username,))
+    try:
+        cur.execute(sql_insert, (username, pwd))
+    except sqlite3.IntegrityError:
+        raise ValueError("User already exists")
     conn.commit()
     print(username, "added successfuly in table")
 
-def delete_user_db (usrname):
-    cur.execute(sql_delete, (usrname,))
+def delete_user_db (username):
+    if not user_found(username):
+        raise ValueError("User not found")
+    cur.execute(sql_delete, (username,))
     conn.commit()
     print("user deleted successfully")
 
-def search_user (username):
-    if user_found:
-        pass
 
-    user, stored_pwd = cur.fetchone()
-    if user is None:
-        print("User nor found")
-        return False
+def search_user (username):
+    if not user_found(username):
+        print("User not found")
+        return None
+    cur.execute(sql_check, (username,))
+    user, stored_pwd = cur.fetchone()[1:3]
     print("User found")
     return user, stored_pwd
 
-def get_users():
+def get_users() -> list:
+    print("Entred get_users")
     cur.execute(sql_select)
     users = cur.fetchall()
     return users
 
-def user_found (username):
+def user_found (username) -> bool:
     cur.execute(sql_check, (username,))
     if cur.fetchone():
         return True
