8cee811cf52b685f8b1d33b1f93d33c36fb24af4
= <=>
2026-05-20 14:41:27 +0200

    Refactored (api): inject db connection into routes.
    Injected the connection as a Dependency in all routes.
    Wrapped register_user im try except to test the error handling.

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from app.db.database import get_users
+from fastapi import APIRouter, HTTPException, Depends
+from app.db.database import get_users, get_db
 from app.models.user import User
 from app.services.user_managment import login_user, register_user, delete_user
 
@@ -10,21 +10,24 @@ def welcome():
     return {"message": "Welcome to the Webapp."}
 
 @router.post("/register/")
-def register(user: User):
-    register_user(user.username, user.password)
+def register(user: User, conn = Depends(get_db)):
+    try:
+        register_user(conn, user.username, user.password)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid username or password")
     return {"new user registred: ": user.username}
 
 @router.post("/login/")
-def login(user: User):
-    login_user(user.username, user.password)
+def login(user: User, conn = Depends(get_db)):
+    login_user(conn, user.username, user.password)
     return {"Login Succeeded."}
 
 @router.get("/users/")
-def users() -> list:
+def users(conn = Depends(get_db)) -> list:
     print("message Welcome to the users page.")
-    return get_users()
+    return get_users(conn)
 
 @router.delete("/user/")
-def delete(user: User):
-    delete_user(user.username, user.password)
+def delete(user: User, conn = Depends(get_db)):
+    delete_user(conn, user.username, user.password)
     return {"deleted user: " : user.username}
