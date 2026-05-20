b4b5857a958ca462b5e7c4111a23b2fd030b9731
= <=>
2026-05-20 23:53:23 +0200

    Refactored (services):
    Introduced UserInput and UserOutput models
    Using models directly in the functions instead of username and password as strings.
    Function now can return a user from type OutputUser.
    Improved error handling in login_user()
    Deleted get_user() we don't need it.

--- app/services/user_managment.py
+++ app/services/user_managment.py
@@ -1,29 +1,23 @@
 from app.cores.hash import hash_password, verify_password
-from app.db.database import add_user_db, search_user, delete_user_db, get_stored_pwd
-from app.services.auth_service import check_password
+from app.db.database import add_user_db, check_username, delete_user_db, get_stored_password
+from app.models.user import UserOutput, UserInput
 
+def register_user(conn, user: UserInput) -> UserOutput:
+    hashed_password = hash_password(user.password)
+    add_user_db(conn, user.username, hashed_password)
+    return UserOutput(username=user.username)
 
-def register_user(conn, username: str, password: str):
-    hashed_password = hash_password(password)
-    add_user_db(conn, username, hashed_password)
+def login_user(conn, user: UserInput) -> UserOutput:
+    if not check_username(conn, user.username):
+        raise ValueError("Username is incorrect.")
+    stored_password = get_stored_password(conn, user.username)
+    if not verify_password(stored_password, user.password) :
+       raise ValueError("Incorrect password.")
+    return UserOutput(username=user.username)
 
-
-def get_user():
-    #TODO: implement later to check if user valid and get its data
-    pass
-
-def login_user(conn, username, pwd):
-    user = search_user(conn, username)
-    stored_pwd = get_stored_pwd(conn, username)
-    if not verify_password(stored_pwd, pwd) :
-       raise ValueError("Username or password is incorrect.")
-    print("Login Succeeded DB")
-    return user
-
-def delete_user(conn, username, pwd):
-    stored_pwd = get_stored_pwd(conn, username)
-    if not verify_password(stored_pwd, pwd):
-def delete_user(conn, username: str, password: str):
+def delete_user(conn, user: UserInput):
+    stored_pwd = get_stored_password(conn, user.username)
+    if not verify_password(stored_pwd, user.password):
         raise ValueError("password is incorrect.")
     print("User deleted DB")
-    delete_user_db(conn, username)
+    delete_user_db(conn, user.username)
