464a80529c9887b27593b75a74b36593966614b2
= <=>
2026-05-20 14:29:47 +0200

    Refactored (Services): adapt user_management.py to db api.
    Functions accept now 'conn' as an argument to work with it locally.
    Seperated searching for username and getting password.
    Call verify_password directly instead of check_password (KEEP IT SIMPLE STUPID)

--- app/services/user_managment.py
+++ app/services/user_managment.py
@@ -1,27 +1,28 @@
-from app.cores.hash import hash_password
-from app.db.database import add_user_db, search_user, delete_user_db
-from app.services.auth_service import check_password, check_username
+from app.cores.hash import hash_password, verify_password
+from app.db.database import add_user_db, search_user, delete_user_db, get_stored_pwd
+from app.services.auth_service import check_password
 
 
-def register_user(username, pwd):
-    hs_pwd = hash_password(pwd)
-    add_user_db(username, hs_pwd)
-    return {"Registration complete DB"}
+def register_user(conn, username, pwd):
+    hashed_password = hash_password(pwd)
+    add_user_db(conn, username, hashed_password)
+
 
 def get_user():
     #TODO: implement later to check if user valid and get its data
     pass
 
-def login_user(username, pwd):
-    user, stored_pwd = search_user(username)
-    if not check_password(stored_pwd, pwd) or check_username(username) == False:
+def login_user(conn, username, pwd):
+    user = search_user(conn, username)
+    stored_pwd = get_stored_pwd(conn, username)
+    if not verify_password(stored_pwd, pwd) :
        raise ValueError("Username or password is incorrect.")
     print("Login Succeeded DB")
     return user
 
-def delete_user(username, pwd):
-    user, stored_pwd = search_user(username)
-    if not check_password(stored_pwd, pwd):
-        print("False password! try again")
+def delete_user(conn, username, pwd):
+    stored_pwd = get_stored_pwd(conn, username)
+    if not verify_password(stored_pwd, pwd):
+        raise ValueError("password is incorrect.")
     print("User deleted DB")
-    delete_user_db(username)
+    delete_user_db(conn, username)
