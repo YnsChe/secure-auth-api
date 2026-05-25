259dbfeb0a64427b2e4d7459beb26ea0642517a5
= <=>
2026-05-25 16:52:24 +0200

    Added constraints to user Model

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr
 
 class UserInput(BaseModel):
-    username: str
-    password: str
+    username: constr(strip_whitespace=True, min_length=3, max_length=20)
+    password: constr(min_length=8)
 
 class UserOutput(BaseModel):
     username: str
