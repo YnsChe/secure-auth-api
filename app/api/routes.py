c7dd4a40ec35166fde7e314676ce3f16a359b4be
Youness Guermah <youness.guermah@gmail.com>
2026-05-29 13:12:36 +0200

    Refactored API (routes.py)
    Put UserLogin/UserRegister where it should be and modified some staus codes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from app.db.database import get_users, get_db
-from app.models.user import UserInput, UserOutput
+from app.models.user import UserRegister, UserOutput, UserLogin
 from app.services.user_managment import login_user, register_user, delete_user
 
 router = APIRouter()
@@ -10,7 +10,7 @@ def welcome():
     return {"message": "Welcome to the Webapp."}
 
 @router.post("/register/")
-def register(user: UserInput, conn = Depends(get_db)):
+def register(user: UserRegister, conn = Depends(get_db)):
     try:
         register_user(conn, user)
     except ValueError as e:
@@ -18,11 +18,11 @@ def register(user: UserInput, conn = Depends(get_db)):
     return UserOutput(username=user.username)
 
 @router.post("/login/")
-def login(user: UserInput, conn = Depends(get_db)):
+def login(user: UserLogin, conn = Depends(get_db)):
     try:
         login_user(conn, user)
     except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=401, detail=str(e))
     return {"Login Succeeded."}
 
 @router.get("/users/")
@@ -34,9 +34,9 @@ def users(conn = Depends(get_db)) -> list:
     return users_list
 
 @router.delete("/user/")
-def delete(user: UserInput, conn = Depends(get_db)):
+def delete(user: UserLogin, conn = Depends(get_db)):
     try:
         delete_user(conn, user)
     except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=401, detail=str(e))
     return {"deleted user: " : user.username}
