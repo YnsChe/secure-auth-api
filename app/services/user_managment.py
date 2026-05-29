b97d2b9962e96f4e105d14502357ff8708a97280
Youness Guermah <youness.guermah@gmail.com>
2026-05-29 13:15:05 +0200

    Refactored Services (user_management.py)
    Added Type hint for Connection
    Improved Error handling and Error messages (Fewer infos for if  either username or password was wrong)
    Put UserRegister or UserLogin where it should be

--- app/services/user_managment.py
+++ app/services/user_managment.py
@@ -1,20 +1,27 @@
+from sqlite3 import Connection
+
 from app.cores.hash import hash_password, verify_password
 from app.db.database import add_user_db, check_username, delete_user_db, get_stored_password
-from app.models.user import UserOutput, UserInput
+from app.models.user import UserOutput, UserRegister, UserLogin
 
-def register_user(conn, user: UserInput) -> UserOutput:
+def register_user(conn: Connection, user: UserRegister) -> UserOutput:
     hashed_password = hash_password(user.password)
-    add_user_db(conn, user.username, hashed_password)
+    try:
+        add_user_db(conn, user.username, hashed_password)
+    except ValueError:
+        raise ValueError("Error while registering")
     return UserOutput(username=user.username)
 
-def login_user(conn, user: UserInput) -> UserOutput:
+def login_user(conn: Connection, user: UserLogin) -> UserOutput:
     check_username(conn, user.username)
     stored_password = get_stored_password(conn, user.username)
-    verify_password(stored_password, user.password)
+    if not verify_password(stored_password, user.password):
+        raise ValueError("Invalid credentials")
     return UserOutput(username=user.username)
 
-def delete_user(conn, user: UserInput):
+def delete_user(conn: Connection, user: UserLogin):
     check_username(conn, user.username)
     stored_password = get_stored_password(conn, user.username)
-    verify_password(stored_password, user.password)
+    if not verify_password(stored_password, user.password):
+        raise ValueError("Invalid credentials")
     delete_user_db(conn, user.username)
