a7aaa51081a85f4eb43e61f1b3782895da67437d
Youness Guermah <youness.guermah@gmail.com>
2026-05-29 13:08:59 +0200

    Refactored Models (user.py)
    Added UserLogin to separate Sign up and Login.
    Removed Constraints from UserLogin for leaking password and username structure.

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,18 @@
 from pydantic import BaseModel, constr
 
-class UserInput(BaseModel):
+'''
+Seperated login and register to avoid returning password 
+constraints while trying to log in and give attacker hints
+about password structure.
+'''
+class UserRegister(BaseModel):
     username: constr(strip_whitespace=True, min_length=3, max_length=20)
     password: constr(min_length=8)
 
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+# Output just for returning as repsonce
 class UserOutput(BaseModel):
     username: str
