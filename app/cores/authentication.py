6cf92c1c952bc7bb9442611e650a76054c8baf0d
Youness Guermah <youness.guermah@gmail.com>
2026-07-22 11:38:10 +0200

    Refactor (Cores)
    Defined building blocks for the token
    
    Added function to create the access token using the encoded username(sub = data) + Key + Algorithm.
    
    Added get_current_user to validate the token on each request and extract the username of the current user (if it s still valid)

--- app/cores/authentication.py
+++ app/cores/authentication.py
@@ -1,11 +1,46 @@
 """ Here JWT Creation and verification (Low level Authentication)"""
-
+from datetime import timedelta, datetime, timezone
+from http.client import HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from fastapi import Depends,HTTPException, status
+import jwt
+from jwt import InvalidTokenError
+
+from app.db.database import check_username
+from app.models.tokens import TokenData
+
+# Build blocks of the Token
+SECRET_KEY = "46b58bd07a60babe3292e8c5f8a9f7aee5b7a055413e15255f8226a97366f29e"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def create_access_token():
-    pass
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
-def decode_token():
-    pass
+def get_current_user(conn, token: Annotated[str, Depends(oauth2_scheme)]):
+    #TODO: Make a error library for all possible errors
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate Credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = check_username(conn, token_data.username)
+    return user
