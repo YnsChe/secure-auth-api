ede77632c56c8dcad53a9e4626eb6b73b36b50ca
Youness Guermah <youness.guermah@gmail.com>
2026-08-04 11:09:38 +0200

    Refactor (services)
    Renamed files:
    user_managment.py --> user_service.py
    
    Renamed function in auth_service:
    login_jwt() --> issue_token()

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,7 +1,7 @@
 """ High Level Authentication, like authenticate_user(); Login()... """
 from datetime import timedelta
 from app.cores.authentication import create_access_token
-from app.cores.hash import verify_password, DUMMY_HASH
+from app.cores.hashing import verify_password, DUMMY_HASH
 from app.db.database import check_username, get_stored_password
 from app.models.tokens import ACCESS_TOKEN_EXPIRE_MINUTES
 from app.models.user import UserLogin, UserInDB
@@ -17,7 +17,7 @@ def authenticate_user(conn, user: UserLogin) -> UserInDB:
         raise ValueError("Invalid Credentials")
     return UserInDB(username=user.username, hashed_password=stored_pwd)
 
-def login_jwt(user: UserInDB):
+def issue_token(user: UserInDB):
     access_token_expires = timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return access_token
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,8 +1,8 @@
 from sqlite3 import Connection
-from app.cores.hash import hash_password
+from app.cores.hashing import hash_password
 from app.db.database import add_user_db,delete_user_db
 from app.models.user import UserOutput, UserRegister, UserLogin
-from app.services.auth_service import authenticate_user, login_jwt
+from app.services.auth_service import authenticate_user, issue_token
 
 """Business logic for user registration, login, and deletion."""
 
@@ -17,7 +17,7 @@ def register_user(conn: Connection, user: UserRegister) -> UserOutput:
 
 def login_user(conn: Connection, user: UserLogin):
     userdb = authenticate_user(conn,user)
-    return login_jwt(userdb)
+    return issue_token(userdb)
 
 
 def delete_user(conn: Connection, user: UserLogin):
