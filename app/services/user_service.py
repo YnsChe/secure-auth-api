939a05e920d55905f6b12af5b59cb7fb07f113ce
Youness Guermah <youness.guermah@gmail.com>
2026-08-11 23:14:12 +0200

    Refactored (Services)
    improved delete_service to return an error when the action is executed by a non admin
    Added a service function to authenticate the logged in user, check if he is an admin and call the user list from the db

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,8 @@
+from operator import truediv
 from sqlite3 import Connection
 from app.cores.hashing import hash_password
-from app.db.database import add_user_db, delete_user_db, get_role
-from app.models.users import UserOutput, UserRegister, UserLogin
+from app.db.database import add_user_db, delete_user_db, get_role, list_users
+from app.models.users import UserOutput, UserRegister, UserLogin, UserInDB
 from app.services.auth_service import authenticate_user, issue_token
 
 """Business logic for user registration, login, and deletion."""
@@ -19,9 +20,25 @@ def login_user(conn: Connection, user: UserLogin):
     userdb = authenticate_user(conn,user)
     return issue_token(userdb)
 
-
 def delete_service(conn: Connection, user: UserLogin, user_name):
     """Delete a user after verifying their credentials."""
     authenticate_user(conn, user)
     if get_role(conn, user.username) == "admin":
-        delete_user_db(conn, user_name)
+        try:
+            delete_user_db(conn, user_name)
+        except ValueError:
+            raise ValueError("Contact an Admin to delete the user")
+
+
+def list_users_service(conn: Connection, user: UserLogin):
+    authenticate_user(conn, user)
+    if get_role(conn, user.username) == "admin":
+        try:
+            list_users(conn)
+        except ValueError:
+            raise ValueError("Contact an Admin to list all users")
+
+"""def check_admin(user: UserInDB)-> bool:
+    if user.role == "admin":
+        return True
+    return False"""
