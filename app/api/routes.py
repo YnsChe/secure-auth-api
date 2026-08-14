fd3efc27388aada4e47e4a211cf59f5f29e60e06
Youness Guermah <223534663+YnsChe@users.noreply.github.com>
2026-08-14 12:55:56 +0200

    Refactored (API)
    Added a route for updating the role fo a user (can be done just from admins)
    Ordered and renamed the routes better

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from app.cores.authentication import get_current_user
 from app.db.database import get_db
 from app.models.users import UserRegister, UserOutput, UserLogin
-from app.services.user_service import register_user, delete_service, login_user, list_users_service
+from app.services.user_service import register_user, delete_service, login_user, list_users_service, update_service
 
 """HTTP API routes for user registration, login, listing, and deletion."""
 router = APIRouter()
@@ -34,25 +34,25 @@ def login(user: UserLogin, conn= Depends(get_db)):
         raise HTTPException(status_code=401, detail=str(e))
     return {"access_token": token, "token_type": "Bearer"}
 
-
-@router.get("/users/me")
-def read_users_me(current_user: Annotated[UserLogin, Depends(get_current_user)]):
-    """Returns logged in user"""
-    return current_user
-
-
-@router.get("/users/")
+# for now as a POST methode because GET with a body did not work and is not recomended.
+# Later when the tokens will be stored in the front end, we ll use the GET Mehtod.
+@router.post("/users/")
 def list_users(user: UserLogin,conn= Depends(get_db)) -> list:
     """Return all users (usernames)."""
+    print("called /users/ route")
     try:
         users_list = list_users_service(conn, user)
     except SystemError as e:
         raise HTTPException(status_code=500, detail=str(e))
     return users_list
 
+@router.get("/user/me")
+def read_users_me(current_user: Annotated[UserLogin, Depends(get_current_user)]):
+    """Returns logged in user"""
+    return current_user
 
 @router.delete("/user/{user_name}")
-def delete_user(user_name, user: UserLogin, conn= Depends(get_db)):
+def delete_user(user_name: str, user: UserLogin, conn= Depends(get_db)):
     """Delete a user after credential verification."""
     try:
         delete_service(conn, user, user_name)
@@ -60,10 +60,15 @@ def delete_user(user_name, user: UserLogin, conn= Depends(get_db)):
         raise HTTPException(status_code=401, detail=str(e))
     return {"deleted user: ": user_name}
 
-@router.put("{user_name}")
-def update_user_role(user_name, user: UserLogin, conn= Depends(get_db)):
-    """Update the role of an existing user"""
-    pass
+@router.put("/user/{user_name}/{role}")
+def update_user_role(user_name: str, role, user: UserLogin, conn= Depends(get_db)):
+    """Update an attribute of an existing user"""
+    print("[+] Entred update_user_role")
+    try:
+        update_service(conn, user, user_name, role)
+    except ValueError as e:
+        raise HTTPException(status_code=401, detail=str(e))
+    return "Role updated successfully"
 
 @router.get("/protected/")
 def protected_endpoint(current_user: Annotated[str, Depends(get_current_user)]) -> str:
