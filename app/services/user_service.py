133379f9df4874744bea9b299877c2dc3de51462
Youness Guermah <223534663+YnsChe@users.noreply.github.com>
2026-08-14 12:56:37 +0200

    Refactored (Services)
    Added an update service to relate the route changes to the database

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,8 +1,7 @@
-from operator import truediv
 from sqlite3 import Connection
 from app.cores.hashing import hash_password
-from app.db.database import add_user_db, delete_user_db, get_role, list_users_db
-from app.models.users import UserOutput, UserRegister, UserLogin, UserInDB
+from app.db.database import add_user_db, delete_user_db, get_role, list_users_db, update_user_db
+from app.models.users import UserOutput, UserRegister, UserLogin
 from app.services.auth_service import authenticate_user, issue_token
 
 """Business logic for user registration, login, and deletion."""
@@ -27,7 +26,7 @@ def delete_service(conn: Connection, user: UserLogin, user_name):
         try:
             delete_user_db(conn, user_name)
         except ValueError:
-            raise ValueError("Contact an Admin to delete the user")
+            raise ValueError("User cannot be deleted")
 
 
 def list_users_service(conn: Connection, user: UserLogin):
@@ -36,7 +35,17 @@ def list_users_service(conn: Connection, user: UserLogin):
         try:
             list_users_db(conn)
         except ValueError:
-            raise ValueError("Contact an Admin to list all users")
+            raise ValueError("List cannot be displayed")
+
+def update_service(conn: Connection, user: UserLogin, user_name: str, role: str):
+    authenticate_user(conn, user)
+    if get_role(conn, user.username) == "admin":
+        try:
+            update_user_db(conn, user_name, role)
+        except ValueError:
+            raise ValueError("User cannot be updated")
+
+
 
 """def check_admin(user: UserInDB)-> bool:
     if user.role == "admin":
